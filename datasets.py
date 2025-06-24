01a52923fc391497a8b1596b791a1f677727fe76
ffazzm <fathin.azzaim@gmail.com>
2025-06-24 11:24:42 +0700

    add BalancedBatchSampler for DF dataset

--- datasets.py
+++ datasets.py
@@ -248,3 +248,50 @@ class BalancedBatchSampler(BatchSampler):
 
     def __len__(self):
         return self.n_dataset // self.batch_size
+    
+class BalancedBatchSamplerFromDF(BatchSampler):
+    """
+    BalancedBatchSampler for custom dataset:
+    Randomly samples n_classes, and for each class samples n_samples.
+    Returns batches of size (n_classes * n_samples).
+    """
+
+    def __init__(self, labels, n_classes, n_samples):
+        # labels: numpy array or pd.Series (e.g. df['model_encoded'].values)
+        self.labels = np.array(labels)
+        self.labels_set = list(set(self.labels))
+        self.label_to_indices = {
+            label: np.where(self.labels == label)[0]
+            for label in self.labels_set
+        }
+        for l in self.labels_set:
+            np.random.shuffle(self.label_to_indices[l])
+
+        self.used_label_indices_count = {label: 0 for label in self.labels_set}
+        self.count = 0
+        self.n_classes = n_classes
+        self.n_samples = n_samples
+        self.n_dataset = len(self.labels)
+        self.batch_size = self.n_classes * self.n_samples
+
+    def __iter__(self):
+        self.count = 0
+        while self.count + self.batch_size <= self.n_dataset:
+            classes = np.random.choice(self.labels_set, self.n_classes, replace=False)
+            indices = []
+            for class_ in classes:
+                start = self.used_label_indices_count[class_]
+                end = start + self.n_samples
+                indices.extend(self.label_to_indices[class_][start:end])
+                self.used_label_indices_count[class_] += self.n_samples
+
+                # Reshuffle if not enough left
+                if self.used_label_indices_count[class_] + self.n_samples > len(self.label_to_indices[class_]):
+                    np.random.shuffle(self.label_to_indices[class_])
+                    self.used_label_indices_count[class_] = 0
+
+            yield indices
+            self.count += self.batch_size
+
+    def __len__(self):
+        return self.n_dataset // self.batch_size
